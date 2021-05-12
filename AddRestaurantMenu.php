366bb7729bb6ab4e48b370f0a0d783d6b0481528
unknown <alexming866@gmail.com>
2021-05-12 11:49:12 +0800

    layout of add restaurant menu

--- AddRestaurantMenu.php
+++ AddRestaurantMenu.php
@@ -1,19 +1,115 @@
 <html>
     <head>
         <title>Add Menu</title>
-        
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="./css/menu.css" >
+        <style>
+            table{
+                border-collapse: separate;
+                border-spacing: 0 15px;
+            }
+            td{
+                padding: 5px;
+            }
+            .tdname{
+                width: 10%;
+            }
+            
+        </style>
     </head>
     <body>
         <?php 
             include "menu.php";
         ?>
-        <div class="container-fluid" style="background-color: red;">
+        <div class="container-fluid" style="width:95%">
+            <div class="row">
+                <div class="col-xl-12 my-auto">
+                    <form method="POST" action="./Alex/AddMenu.php">
+                        <table border="0" style="width: 100%;">
+                            <tr>
+                                <td colspan="2" style="height: 300px;">
+                                    <div id="upload-image preview">
 
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="inputGroupFile02">
+                                            <label class="custom-file-label" for="inputGroupFile02">Choose file</label>
+                                        </div>
+                                        <div class="input-group-append">
+                                            <span class="input-group-text" id="">Upload</span>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="tdname">Menu Name</td>
+                                <td>
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="tdname">Price</td>
+                                <td>
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="tdname">Ingredients</td>
+                                <td>
+                                    <div class="input-group">
+                                        <textarea class="form-control" aria-label="With textarea" style="height: 150px;"></textarea>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="tdname">Description</td>
+                                <td>
+                                    <div class="input-group">
+                                        <textarea class="form-control" aria-label="With textarea" style="height: 150px;"></textarea>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2" style="text-align: center;">
+                                    <button type="submit" class="btn btn-md btn-primary" value="restaurant-submit">Submit</button>
+                                    <button type="reset" class="btn btn-md btn-primary" value="restaurant-reset">Clear</button>
+                                </td>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
+                <div class="col-xl-12">
+                    <footer class="page-footer font-small teal pt-4">
+                        <div class="container-fluid text-center text-md-left">
+                            <div class="row">
+                                <div class="col-md-6 mt-md-0 mt-3">
+                                    <h5 class="text-uppercase font-weight-bold">Footer text 1</h5>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita sapiente sint, nulla, nihil
+                                    repudiandae commodi voluptatibus corrupti animi sequi aliquid magnam debitis, maxime quam recusandae
+                                    harum esse fugiat. Itaque, culpa?</p>
+                                </div>
+                                <hr class="clearfix w-100 d-md-none pb-3">
+                                <div class="col-md-6 mb-md-0 mb-3">
+                                    <h5 class="text-uppercase font-weight-bold">Footer text 2</h5>
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio deserunt fuga perferendis modi earum
+                                    commodi aperiam temporibus quod nulla nesciunt aliquid debitis ullam omnis quos ipsam, aspernatur id
+                                    excepturi hic.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                            <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+                        </div>
+                    </footer>
+                </div>
+            </div>
         </div>
     </body>
 </html>
