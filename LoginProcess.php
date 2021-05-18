bbaf0cd2057b1c16f55cc62348ba8aee3b0740fd
Vernon-C <vernonon0530@gmail.com>
2021-05-19 00:36:17 +0800

    Changed php files to adhere to oop standards and added test file

--- LoginProcess.php
+++ LoginProcess.php
@@ -1,30 +1,41 @@
 <?php
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    $username = validate($_POST['username']);
-    $password = validate($_POST['password']);
 
-    // Check if any fields are empty
-    if (empty($username)) {
-        header("Location: login.php?error=Please enter a username");
-        exit();
-    } else if (empty($password)) {
-        header("Location: login.php?error=Please enter a password");
-        exit();
-    } else {
-        echo "Valid";
+class LoginProcess {
+    private $username;
+    private $password;
+
+    public function validateDetails($username, $password) {
+        // Check if any fields are empty
+        if (empty($username)) {
+            header("Location: login.php?error=Please enter a username");
+            exit();
+        } else if (empty($password)) {
+            header("Location: login.php?error=Please enter a password");
+            exit();
+        } else {
+            echo "Valid";
+        }
     }
 
+    // Removes spaces, slashes, and special characters
+    public function trimData($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+
+        return $data;
+    }
+}
+
+$validate = new LoginProcess();
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    //$username = $validate->trimData($_POST['username']);
+    //$password = $validate->trimData($_POST['password']);
+    //$validate->validateDetails($username, $password);
 } else {
-    header("Location: login.php");
+    header("Location: Login.php");
     exit();
 }
 
-// Removes spaces, slashes, and special characters
-function validate($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-
-    return $data;
-}
 ?>
