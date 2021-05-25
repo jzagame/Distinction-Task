80945ae9797da86a0936bf172bbcf131e4eefed8
Vernon-C <vernonon0530@gmail.com>
2021-05-25 17:18:49 +0800

    Added LoginProcessTest

--- tests/LoginProcessTest.php
+++ tests/LoginProcessTest.php
@@ -6,6 +6,15 @@ class LoginProcessTest extends TestCase {
     public function testValidateDetails() {
         $loginProcess = new LoginProcess();
         $this->assertEquals("Valid", $loginProcess->validateDetails("Vernon", "12345"));
+        $this->assertEquals("Please enter a username", $loginProcess->validateDetails("", "12345"));
+        $this->assertEquals("Please enter a password", $loginProcess->validateDetails("Vernon", ""));
+    }
+
+    public function testTrimData() {
+        $loginProcess = new LoginProcess();
+        $this->assertEquals("Vernon", $loginProcess->trimData("  Vernon  "));
+        $this->assertEquals("Vernon's", $loginProcess->trimData("Vern\on\'s"));
+        $this->assertEquals("&lt;b&gt;Vernon&lt;/b&gt;", $loginProcess->trimData("<b>Vernon</b>"));
     }
 }
 
