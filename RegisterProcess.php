c52f80805ceff22368e8cc9a9402154e22c5fce0
Vernon-C <vernonon0530@gmail.com>
2021-05-25 18:28:58 +0800

    Added validation for login page

--- RegisterProcess.php
+++ RegisterProcess.php
@@ -5,10 +5,16 @@ class RegisterProcess {
         // Check if any fields are empty
         if (empty($email)) {
             return "Please enter your email address";
+        } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {  // Email validation
+            return "Invalid email address. Please try again";
         } else if (empty($username)) {
             return "Please enter a username";
+        } else if (strlen($username) < 5) {
+            return "Username is too short. Please try again";
         } else if (empty($password)) {
             return "Please enter a password";
+        } else if (strlen($password) < 5) {
+            return "Password is too short. Please try again.";
         } else if (empty($confirm_password)) {
             return "Please enter your confirmed password";
         } else if ($password != $confirm_password) {
--- index.php
+++ index.php
@@ -17,10 +17,6 @@ h1, h2, h3, h4, h5, h6 {
 
 session_start();
 
-if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
-  
-}
-
 ?>
 
 <!-- Navbar (sit on top) -->
